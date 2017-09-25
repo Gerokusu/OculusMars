0cb49c69b4699f4c59bb14c36b32a719881a83c4
Grox2006 <thibaud.laurent6@gmail.com>
2017-09-25 15:56:25 +0200

    [0.0.6] Ajout d'étoiles et de la rotation de planète dans le menu principal

---
+++ OculusMarsSimulation/Assets/World/Planets/Scripts/PlanetSpin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlanetSpin : MonoBehaviour
+{
+    public int RotationSpeed = 1;
+
+	public void FixedUpdate()
+    {
+        transform.Rotate(0, -RotationSpeed * Time.deltaTime, 0);
+	}
+}
